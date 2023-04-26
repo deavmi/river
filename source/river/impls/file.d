453d8cf517dc4dba5693233edb3ab3e46aae28eb
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-04-26 08:51:32 +0200

    File
    - Added empty module

---
+++ source/river/impls/file.d
@@ -0,0 +1,23 @@
+module river.impls.file;
+
+import river.core;
+
+// import std.stdio;
+
+// public class FileStream : Stream
+// {
+//     /** 
+//      * Underlying file
+//      */
+//     private File file;
+
+//     this(File file)
+//     {
+//         this.file = file;
+//     }
+
+//     public override ulong read(ref byte[] toArray)
+//     {
+//         // file.rawRead()
+//     }
+// }
