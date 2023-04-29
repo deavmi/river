8bdb607eeef5dfd377f9dc7dc65f2e118c162728
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-04-29 16:02:23 +0200

    - Created an `impls` package

---
+++ source/river/impls/package.d
@@ -0,0 +1,14 @@
+/**
+ * Several batteries-included stream implementations ready for use
+ */
+module river.impls;
+
+/** 
+ * Socket stream
+ */
+public import river.impls.sock;
+
+/** 
+ * Pipe-based stream
+ */
+public import river.impls.pipe;
