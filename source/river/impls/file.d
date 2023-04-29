4bb71cd37ca3608b010382615712def68857d33f
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-04-29 16:23:20 +0200

    File
    - Cleaned up commented-out code for now

--- source/river/impls/file.d
+++ source/river/impls/file.d
@@ -1,23 +1,2 @@
 module river.impls.file;
 
-import river.core;
-
-// import std.stdio;
-
-// public class FileStream : Stream
-// {
-//     /** 
-//      * Underlying file
-//      */
-//     private File file;
-
-//     this(File file)
-//     {
-//         this.file = file;
-//     }
-
-//     public override ulong read(ref byte[] toArray)
-//     {
-//         // file.rawRead()
-//     }
-// }
