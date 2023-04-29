7b226c5ab79af9c90bb02b7530887cfa1ffca66f
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-04-29 16:23:08 +0200

    FDStream
    - Added new base-stream type for streams that are constructed from file descriptors

---
+++ source/river/impls/fd.d
@@ -0,0 +1,57 @@
+module river.impls.fd;
+
+import river.core;
+
+/** 
+ * Provides a base for streams based on file descriptor
+ * pairs (a read fd and a write fd; where they can be
+ * the same fd)
+ */
+public abstract class FDStream : Stream
+{
+    /** 
+     * Read/Write file descriptors
+     */
+    protected const int readEndFd, writeEndFd;
+
+    /** 
+     * Creates a new FDStream with the given read and write
+     * file descriptors (they may be the same)
+     *
+     * Params:
+     *   readEndFd = the file descriptor to read from
+     *   writeEndFd = the file descriptor to write to
+     */
+    this(int readEndFd, int writeEndFd)
+    {
+        this.readEndFd = readEndFd;
+        this.writeEndFd = writeEndFd;
+    }
+
+    /**
+     * Closes the file descriptors
+     */
+    public override void close()
+    {
+        version(Posix)
+        {
+            import core.sys.posix.unistd : close;
+
+            // TODO: Do something with the error code of both calls to `close`
+            if(readEndFd == writeEndFd)
+            {
+                close(readEndFd);
+            }
+            else
+            {
+                close(readEndFd);
+                close(writeEndFd);
+            } 
+        }
+        else
+        {
+            pragma(msg, "FDStream: The close() call is not implemented for your platform");
+            static assert(false);
+        }
+    }
+}
