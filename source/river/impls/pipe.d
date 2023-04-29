a9f531b24a7deb4ed162462e2525981aa061b2e7
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-04-29 13:12:04 +0200

    PipeStream
    - Implemented `read(ref byte[])`

--- source/river/impls/pipe.d
+++ source/river/impls/pipe.d
@@ -1,14 +1,40 @@
 module river.impls.pipe;
 
-// import river.core;
+import river.core;
 
-// import std.file;
+import std.stdio : File;
+import std.exception : ErrnoException;
 
-// public class Pipe : Stream
-// {
-    
-//     this()
-//     {
+public class Pipe : Stream
+{
+    /** 
+     * Pipe endpoints
+     */
+    private File readEnd, writeEnd;
 
-//     }
-// }
+    /** 
+     * Constructs a new piped-stream with the file descriptors of the
+     * read and write ends provided
+     *
+     * Params:
+     *   readEnd = the fd of the pipe's read end
+     *   writeEnd = the fd of the pipe's write end
+     */
+	this(int readEnd, int writeEnd)
+	{
+        this.readEnd.fdopen(readEnd);
+        this.writeEnd.fdopen(writeEnd);
+	}
+
+    public override ulong read(ref byte[] toArray)
+    {
+        try
+        {
+            return readEnd.rawRead(toArray).length;
+        }
+        catch(ErrnoException fileError)
+        {
+            throw new StreamException(StreamError.OPERATION_FAILED);
+        }
+    }
+}
