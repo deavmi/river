6bca625f2e89a272bc7a02d59b1ac342eb0298e4
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-04-29 16:54:24 +0200

    - New stream type added `FileStream`

--- source/river/impls/file.d
+++ source/river/impls/file.d
@@ -1,2 +1,15 @@
 module river.impls.file;
 
+import river.impls.fd : FDStream;
+import std.stdio : File;
+
+/**
+ * `File`-backed stream
+ */
+public class FileStream : FDStream
+{
+    this(File file)
+    {
+        super(file.fileno());
+    }
+}
--- source/river/impls/package.d
+++ source/river/impls/package.d
@@ -9,6 +9,16 @@ module river.impls;
 public import river.impls.sock;
 
 /** 
+ * FD-based stream
+ */
+public import river.impls.fd;
+
+/** 
  * Pipe-based stream
  */
 public import river.impls.pipe;
+
+/** 
+ * `File`-based stream
+ */
+public import river.impls.file;
