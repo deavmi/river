440589fbd00be5689170221c4e327af39919735d
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-04-29 20:57:14 +0200

    MemoryStream
    - Added some pseudo code/ideas

---
+++ source/river/impls/memory.d
@@ -0,0 +1,26 @@
+module river.impls.memory;
+
+import river.core;
+
+
+
+import core.sync.mutex : Mutex;
+// import core.sys.posix.sys.mman;
+
+// TODO: Look at io-uring too, could be cool
+
+/** 
+ * TODO: We ought to do this in an efficient manner
+ */
+public class MemoryStream : Stream
+{
+    private Mutex memoryLock;
+
+    private ulong 
+
+    this()
+    {
+
+    }
+    
+}
