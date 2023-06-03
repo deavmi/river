16e155764794b56e5dc9061579c03875b6d68bad
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-06-03 12:14:58 +0200

    Peakable
    - Defined the method `peak(byte[])

--- source/river/core/peakable.d
+++ source/river/core/peakable.d
@@ -8,5 +8,18 @@ module river.core.peakable;
  */
 public interface Peakable
 {
-    
+    /** 
+     * Reads bytes from the stream into the provided array
+     * and returns without any further waiting, at most the
+     * number of bytes read will be the length of the provided
+     * array, at minimum a single byte.
+     *
+     * The underlying buffer of the `Stream` will not have
+     * said bytes removed from it however.
+     *
+     * Params:
+     *   toArray = the buffer to read into
+     * Returns: the number of bytes read 
+     */
+    public ulong peak(byte[] toArray);
 }
